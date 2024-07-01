7454061f68f6bf3b7bb001da99c0495a90ecb6db
EthanMunoz03 <142924410+EthanMunoz03@users.noreply.github.com>
2024-06-30 22:35:50 -0500

    Day 3

--- project.py
+++ project.py
@@ -1,5 +1,7 @@
 # spotify project i think
 import requests
+import pandas as pd
+import sqlalchemy as db
 
 CLIENT_ID = "d9521da5b9f840378f1935e0edbb2517"
 CLIENT_SECRET = "d544ef490e1b46c9a04e9757df1a60c3"
@@ -20,3 +22,13 @@ r = requests.get(BASE_URL + 'audio-features/' + track_id, headers=headers)
 
 data = r.json()
 print(data)
+
+# Day 3
+
+tbl = pd.DataFrame.from_dict(details, orient='columns')
+engine = db.create_engine('sqlite:///tbl.db')
+tbl.to_sql('spotify_stats', con=engine, if_exists='replace', index=False)
+
+with engine.connect() as connection:
+   query_result = connection.execute(db.text("SELECT * FROM table_name;")).fetchall()
+   print(pd.DataFrame(query_result))
